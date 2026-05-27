55fac2f7b44ddf5602277120c6b17b8df5ed3aec
hbktq98brn-stack <alek.pro2014@gmail.com>
2026-05-27 15:24:10 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dashbord/', // ЗАМЕНИТЕ на имя вашего репозитория
+  base: '/dashbord/',   // ← обязательно слэши до и после имени
 })
